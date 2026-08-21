276716ac12b2a5c6247e5bf3c9012960879c32a4
Brad Travers <bradtravers8740@gmail.com>
2026-08-21 12:58:44 -0400

    Add API routes

---
+++ routes/cart.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const cart = require('../controller/cart');
+
+router.get('/', cart.getAllCarts);
+router.get('/:id', cart.getSingleCart);
+router.get('/user/:userid', cart.getCartsbyUserid);
+router.post('/', cart.addOrUpdateCart);
+router.put('/:id', cart.editCart);
+router.patch('/:id', cart.editCart);
+router.delete('/:id', cart.deleteCart);
+
+module.exports = router;
